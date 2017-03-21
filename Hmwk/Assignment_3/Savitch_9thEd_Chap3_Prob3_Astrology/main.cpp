293298381fc29922614491b5c7832dad99cf477c
Seth Tyler <styler5@student.rcc.edu>
2017-03-20 22:55:18 -0700

    problem 3 Signed-off-by: Seth Tyler <styler5@student.rcc.edu>

---
+++ Hmwk/Assignment_3/Savitch_9thEd_Chap3_Prob3_Astrology/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    int day, month;
+    char done;
+    bool cusp = false;
+    
+    do {
+        
+        cout << "Type in your birthday month. (1-12)\n";
+        cin >> month;
+        cout << "Type in your birthday day.\n";
+        cin >> day;
+        
+        if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+            cusp = (month == 3 && day <= 22) || (month == 4 && day >= 18);
+            cout << "You are an Aries.\n";
+        } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+            cusp = (month == 4 && day <= 21) || (month == 5 && day >= 19);
+            cout << "You are a Taurus.\n";
+        } else if ((month == 5 && day >= 21) || (month == 6 && day <= 21)) {
+            cusp = (month == 5 && day <= 22) || (month == 6 && day >= 20);
+            cout << "You are a Gemini.\n";
+        } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) { 
+            cusp = (month == 6 && day <= 22) || (month == 7 && day >= 21);
+            cout << "You are a Cancer.\n";
+        } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+            cusp = (month == 7 && day <= 24) || (month == 8 && day >= 21);
+            cout << "You are a Leo.\n";
+        } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {  
+            cusp = (month == 8 && day <= 24) || (month == 8 && day >= 21);
+            cout << "You are a Virgo.\n";
+        } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+            cusp = (month == 9 && day <= 24) || (month == 10 && day >= 21);
+            cout << "You are a Libra.\n";
+        } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+            cusp = (month == 10 && day <= 24) || (month == 11 && day >= 20);
+            cout << "You are a Scorpio.\n";
+        } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+            cusp = (month == 11 && day <= 23) || (month == 12 && day >= 20);
+            cout << "You are a Sagittarius.\n";
+        } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) { 
+            cusp = (month == 12 && day <= 23) || (month == 1 && day >= 18);
+            cout << "You are a Capricorn.\n";
+        } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+            cusp = (month == 1 && day <= 21) || (month == 1 && day >= 17);
+            cout << "You are an Aquarius.\n";
+        } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+            cusp = (month == 2 && day <= 20) || (month == 3 && day >= 19);
+            cout << "You are a Pisces.\n";
+        }
+        
+        cout << (cusp ? "You are on the cusp of another sign.\n" : "\n");
+        
+        cout << "Are you done? (Y/N)\n";
+        cin >> done;
+        
+    } while (done != 'Y' && done != 'y');
+    
+    return 0;
+}
+
