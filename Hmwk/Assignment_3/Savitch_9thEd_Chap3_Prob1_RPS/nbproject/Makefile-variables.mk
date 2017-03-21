127c99768dac713042b592ab7ecb1b67fbff538b
Seth Tyler <styler5@student.rcc.edu>
2017-03-20 22:53:51 -0700

    problem 3

--- Hmwk/Assignment_3/Savitch_9thEd_Chap3_Prob1_RPS/nbproject/Makefile-variables.mk
+++ Hmwk/Assignment_3/Savitch_9thEd_Chap3_Prob1_RPS/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=default-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/default-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=savitch_9thed_chap3_prob1_rps
-CND_ARTIFACT_PATH_Debug=dist/Debug/default-Windows/savitch_9thed_chap3_prob1_rps
-CND_PACKAGE_DIR_Debug=dist/Debug/default-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/savitch_9thed_chap3_prob1_rps
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=savitch9thedchap3prob1rps.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/default-Windows/package/savitch9thedchap3prob1rps.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/savitch9thedchap3prob1rps.tar
 # Release configuration
-CND_PLATFORM_Release=default-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/default-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=savitch_9thed_chap3_prob1_rps
-CND_ARTIFACT_PATH_Release=dist/Release/default-Windows/savitch_9thed_chap3_prob1_rps
-CND_PACKAGE_DIR_Release=dist/Release/default-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/savitch_9thed_chap3_prob1_rps
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=savitch9thedchap3prob1rps.tar
-CND_PACKAGE_PATH_Release=dist/Release/default-Windows/package/savitch9thedchap3prob1rps.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/savitch9thedchap3prob1rps.tar
 #
 # include compiler specific variables
 #
