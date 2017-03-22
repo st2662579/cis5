4db8af4f838d7c1592895c079cab7438393e825f
Seth Tyler <styler5@student.rcc.edu>
2017-03-22 13:00:31 -0700

    TruthTable Lab Signed-off-by: Seth Tyler <styler5@student.rcc.edu>

---
+++ Lab/TruthTable/main.cpp
@@ -0,0 +1,62 @@
+/* 
+ * File:   main.cpp
+ * Author: Seth Tyler
+ * Created on February 14, 2017, 11:32 AM
+ * Purpose:  Truth Table
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    bool x,y;
+    
+    //Output the table Header
+    cout<<"X Y !X !Y X||Y X&&Y X^Y X^Y^Y X^Y^X !(X||Y) !X&&!Y !(X&&Y) !X||!Y"<<endl;
+    
+    //Determine first row in the table
+    x=true;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"       ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<"       ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<endl;
+    
+    //Determine second row in the table
+    x=true;y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"       ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<"       ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
