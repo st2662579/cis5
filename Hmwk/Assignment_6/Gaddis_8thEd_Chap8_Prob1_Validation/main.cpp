e00037368336a1df0388e2165baa9d42455e44b6
Seth Tyler <styler5@student.rcc.edu>
2017-05-15 18:16:47 -0700

    Signed-off-by: Seth Tyler <styler5@student.rcc.edu>

--- Hmwk/Assignment_6/Gaddis_8thEd_Chap8_Prob1_Validation/main.cpp
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chap8_Prob1_Validation/main.cpp
@@ -1,25 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
- * Author: Seth
- *
- * Created on May 15, 2017, 6:07 PM
+ * Author: Seth Tyler
+ * Created on May 14, 6:02 PM
+ * Purpose:  Problem 6
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>  //Input - Output Library
 
-using namespace std;
+using namespace std; //Name-space under which system libraries exist
 
-/*
- * 
- */
+//User Libraries
+
+//Global Constants
+
+
+//Execution begins here
 int main(int argc, char** argv) {
-
+   
+    //Declare variables
+    int validNums[18] = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 8080152, 4562555, 5552012, 5050552, 7825877, 1250255, 1005231, 6545231, 3852085, 7576651, 7881200, 4581002};
+    int userNum = 0;
+    bool valid = false;
+    
+    cout << "Enter an account number: ";
+    cin >> userNum;
+    
+    for (int i = 0; i < 18; i++) {
+        if (validNums[i] == userNum) {
+            valid = true;
+        }
+    }
+    
+    cout << "\nThis number is " << (valid ? "valid" : "invalid") << "!\n";
+    
+    //Exit stage right!
     return 0;
 }
-
