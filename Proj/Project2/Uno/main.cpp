d2d851a34b76d007a454876fa56d398b9107efa0
Seth Tyler <styler5@student.rcc.edu>
2017-06-02 15:26:59 -0700

    Signed-off-by: Seth Tyler <styler5@student.rcc.edu>

--- Proj/Project2/Uno/main.cpp
+++ Proj/Project2/Uno/main.cpp
@@ -27,31 +27,33 @@ int main(int argc, char** argv) {
     bool playAgain = false; // if true, the loop will continue
     char repeat; // user input for settings playAgain to true or false
 
-    int number[] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
-    vector<vector<int>> cards;
+    int number[19] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
+    vector<vector<int> > cards;
     
-    for (int colors = 1; colors<4; colors++) {
+    for (int colors = 1; colors<=4; colors++) {
         /*
          Red = 1;
          Green = 2;
          Yellow = 3;
          Blue = 4;
          */
-        for (int n = 0; n<17; n++) {
-            vector<int> newCard;
+        for (int n = 0; n<=19; n++) {
+            vector<int> newCard(2);
             newCard.push_back(colors);
             newCard.push_back(number[n]);
             cards.push_back(newCard);
+            newCard.clear();
         }
     }
     
     for (int i = 0; i<cards.size(); i++) {
-        cout << cards[i][0] << " " << cards[i][1];
+        cout << cards[i][0] << " " << cards[i][1] << endl;
     }
     
-    do {
-        
-        
+    
+    
+   /*
+    do {    
         
         // Ask the user if they want to play again.
         cout << "Play again? (Y/N)\n";
@@ -63,5 +65,6 @@ int main(int argc, char** argv) {
         }
     } while (playAgain == true);
 
+    */
     return 0;
 }
