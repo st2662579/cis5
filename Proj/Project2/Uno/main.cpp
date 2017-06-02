002fe7581770d16b8b6d7801d5d79580f015fc00
Seth Tyler <styler5@student.rcc.edu>
2017-06-02 13:58:42 -0700

    Signed-off-by: Seth Tyler <styler5@student.rcc.edu>

--- Proj/Project2/Uno/main.cpp
+++ Proj/Project2/Uno/main.cpp
@@ -1,25 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
- * Author: Seth
- *
- * Created on June 2, 2017, 11:51 AM
+ * Author: Seth Tyler
+ * Created on June 2, 11:54 AM
+ * Purpose:  Uno
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>  //Input - Output Library
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <vector>
 
-using namespace std;
+using namespace std; //Name-space under which system libraries exist
 
-/*
- * 
- */
+//User Libraries
+
+//Function Prototypes
+
+//Execution begins here
 int main(int argc, char** argv) {
+    
+    // Declare variables
+    
+    bool playAgain = false; // if true, the loop will continue
+    char repeat; // user input for settings playAgain to true or false
+
+    int number[] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
+    vector<vector<int>> cards;
+    
+    for (int colors = 1; colors<4; colors++) {
+        /*
+         Red = 1;
+         Green = 2;
+         Yellow = 3;
+         Blue = 4;
+         */
+        for (int n = 0; n<17; n++) {
+            vector<int> newCard;
+            newCard.push_back(colors);
+            newCard.push_back(number[n]);
+            cards.push_back(newCard);
+        }
+    }
+    
+    for (int i = 0; i<cards.size(); i++) {
+        cout << cards[i][0] << " " << cards[i][1];
+    }
+    
+    do {
+        
+        
+        
+        // Ask the user if they want to play again.
+        cout << "Play again? (Y/N)\n";
+        cin >> repeat;
+        if (repeat == 'Y' || repeat == 'y') {
+            playAgain = true;
+        } else {
+            playAgain = false;
+        }
+    } while (playAgain == true);
 
     return 0;
 }
-
